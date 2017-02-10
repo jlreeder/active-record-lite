032a64b97914d7d3c10304b9dfd065103f56c838
Jason Reeder <info@jasonreeder.com>
2017-02-10 14:15:58 -0800

    Add method

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    matches = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        cats
+      WHERE
+        name = 'Breakfast'
+    SQL
+
+    matches.map { |res| new(res) }
   end
 end
 
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end
