270b9bd9d5de437e38f2f4c2b61f5c59219ff9d8
Jason Reeder <info@jasonreeder.com>
2017-02-10 14:29:54 -0800

    Add method

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -20,7 +20,9 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.camelcase
   end
 end
 
