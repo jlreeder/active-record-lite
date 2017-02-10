90eb22b4213310a538105306cf55ba61db9cc485
Jason Reeder <info@jasonreeder.com>
2017-02-10 10:28:59 -0800

    Replace interpolation with .to_sym
    Rubocop style

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,7 +1,7 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |name|
-      define_method "#{name}" do
+      define_method name.to_sym do
         instance_variable_get("@#{name}")
       end
 
