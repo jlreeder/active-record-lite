c8e7d2a9fa0acf587c2a7e4cb3145d05cd852bd4
Jason Reeder <info@jasonreeder.com>
2017-02-10 10:28:29 -0800

    Correct syntax for _get

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -2,7 +2,7 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |name|
       define_method "#{name}" do
-        instance_variable_get(name)
+        instance_variable_get("@#{name}")
       end
 
       define_method "#{name}=" do |value|
